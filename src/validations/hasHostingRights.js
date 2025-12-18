3279a96349c9cb03d6300e823afb5ba2a77fda4a
OscarStol12 <oscar.stolarczyk@yahoo.com>
2025-12-18 12:32:59 -0500

    Add other missing things

---
+++ src/validations/hasHostingRights.js
@@ -0,0 +1,28 @@
+const { EmbedBuilder, Colors, MessageFlags } = require('discord.js');
+const config = require(`${PROJECT_ROOT}/config.json`);
+
+module.exports = async ({interaction, commandObj}) => {
+    if (commandObj && commandObj.validations) {
+        if (commandObj.validations.hasHostingRights) {
+            let userRoles = interaction.user.roles;
+            let canUseCmd = (
+                userRoles.cache.has(config.clearances['Operation-Leading']) ||
+                userRoles.cache.has(config.units.HQASOC) ||
+                userRoles.cache.has(config.units.SWCS) ||
+                userRoles.cache.has(config.units.SFOI) || 
+                userRoles.cache.has(config.units.NCOA) || 
+                userRoles.cache.has(config.units.DOTD)
+            );
+        
+            if (!canUseCmd) {
+                let embed = new EmbedBuilder()
+                .setTitle(`⛔ Access Denied`)
+                .setDescription(`You do not have the proper permissions to use this command.`)
+                .setColor(Colors.Black)
+                .setTimestamp();
+
+                await interaction.reply({embeds: [embed], flags: MessageFlags.Ephemeral});
+            }
+        }
+    }
+}
